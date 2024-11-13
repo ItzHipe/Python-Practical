4b11cc9d853fab4bd236dec4cd5e599a9bb0bc22
Hipe <itzhippeyboi@gmail.com>
2024-11-13 19:41:03 +0530

    Update 8. Program to Print nth Fibonacci Number (N Given by User).py

--- 8. Program to Print nth Fibonacci Number (N Given by User).py
+++ 8. Program to Print nth Fibonacci Number (N Given by User).py
@@ -1,8 +1,11 @@
-def fibonacci(n):
-    if n <= 1:
-        return n
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
-
 n = int(input("Enter the position of the Fibonacci number to find: "))
-print("Fibonacci number at position", n, "is", fibonacci(n))
+
+if n <= 0:
+    print("Please enter a positive integer")
+elif n == 1:
+    print("Fibonacci number at position 1 is 0")
+else:
+    a, b = 0, 1
+    for _ in range(2, n):
+        a, b = b, a + b
+    print("Fibonacci number at position", n, "is", b)
