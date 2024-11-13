641ffdc2ed16dc7544b758f79b6d9389d9c9aaf4
Hipe <itzhippeyboi@gmail.com>
2024-11-13 19:43:29 +0530

    Update 10. Program to Implement Linear Search on 20 Random Generated Numbers.py

--- 10. Program to Implement Linear Search on 20 Random Generated Numbers.py
+++ 10. Program to Implement Linear Search on 20 Random Generated Numbers.py
@@ -1,8 +1,11 @@
 import random
 
 numbers = random.sample(range(1, 100), 20)
+print("List:", numbers)
 target = int(input("Enter a number to search for: "))
 
 found = target in numbers
-print("List:", numbers)
-print(f"{'Found' if found else 'Not found'} the target {target}")
+if found:
+    print("Number found at position", numbers.index(target))
+else:
+    print("Number not found")
